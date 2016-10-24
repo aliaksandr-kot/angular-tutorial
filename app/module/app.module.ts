27d087d2cb9326736e5eb2253abc56503f72f9e9
Aliaksandr Kot <aliaksandr.kot@exposit-ds.com>
2016-10-24 16:03:43 +0300

    http mock

--- app/module/app.module.ts
+++ app/module/app.module.ts
@@ -1,20 +1,25 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import {HeroDetailComponent} from "../component/hero-detail/hero-detail.component";
-
-import {AppComponent} from '../component/app/app.component';
-import {HeroesComponent} from "../component/heroes/heroes.component";
-import {HeroService} from "../service/hero.service";
-import {DashboardComponent} from "../component/dashboard/dashboard.component";
 import {AppRoutingModule} from "../route/app-routing.module";
 
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "../mock/in-memory-data.service";
+
+import {AppComponent} from '../component/app/app.component';
+import {DashboardComponent} from "../component/dashboard/dashboard.component";
+import {HeroesComponent} from "../component/heroes/heroes.component";
+import {HeroDetailComponent} from "../component/hero-detail/hero-detail.component";
+import {HeroService} from "../service/hero.service";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [
--- app/service/hero.service.ts
+++ app/service/hero.service.ts
@@ -1,13 +1,29 @@
 import {Injectable} from "@angular/core";
-import {HEROES} from "../mock/mock-heroes"
+import {Http} from "@angular/http";
+
+import 'rxjs/add/operator/toPromise';
+
 import {Hero} from "../domain/hero";
 
 @Injectable()
 export class HeroService {
 
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
